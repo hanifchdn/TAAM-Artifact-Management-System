038b8ec83216a1f211b8581e95d2ae6687765072
LackingCuriosity <123216154+LackingCuriosity@users.noreply.github.com>
2026-07-15 19:33:09 -0400

    feat: added artifact class/model and add query

---
+++ app/src/main/java/com/example/b07demosummer2024/DatabaseAdder.java
@@ -0,0 +1,6 @@
+package com.example.b07demosummer2024;
+
+public interface DatabaseAdder<Element>{
+    public void addToDatabase(Element e);
+
+}
---
+++ app/src/main/java/com/example/b07demosummer2024/DatabaseUpdater.java
@@ -0,0 +1,6 @@
+package com.example.b07demosummer2024;
+
+public interface DatabaseUpdater<Element> {
+
+    public void updateDatabase(Element e);
+}
