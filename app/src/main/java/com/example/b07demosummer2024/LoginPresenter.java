7ee08eb869041962274a43c3d0958a10467b7071
sherina wijaya <sherina@sherina-wijayas-MacBook-Pro.local>
2026-07-16 16:19:31 -0400

    feat: validate email format

--- app/src/main/java/com/example/b07demosummer2024/LoginPresenter.java
+++ app/src/main/java/com/example/b07demosummer2024/LoginPresenter.java
@@ -6,9 +6,18 @@ public class LoginPresenter implements LoginContract.Presenter {
     public LoginPresenter(LoginContract.View view){
         this.view = view;
     }
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    private boolean isValidEmail(String email){
+        return email.matches(EMAIL_REGEX);
+    }
     @Override
     public void login(String email, String password){
 
+        if(!isValidEmail(email)){
+            view.showInvalidEmailFormatError();
+            return;
+        }
     }
     @Override
     public void onDestroy(){
