88e5585fe91850c10c3f9c075b434ef4afeb9062
sherina wijaya <sherina200607@gmail.com>
2026-07-19 15:09:54 -0400

    feat: add username, email, and password validation

--- app/src/main/java/com/example/b07demosummer2024/SignUpPresenter.java
+++ app/src/main/java/com/example/b07demosummer2024/SignUpPresenter.java
@@ -7,9 +7,35 @@ public class SignUpPresenter implements SignUpContract.Presenter {
 
         this.view = view;
     }
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    private boolean isValidEmail(String email){
+        return email.matches(EMAIL_REGEX);
+    }
     @Override
     public void signUp(String email, String password, String confirmPassword){
+        if (email.isEmpty()) {
+            view.showEmptyEmailError();
+            return;
+        }
+        if(!isValidEmail(email)){
+            view.showInvalidEmailFormatError();
+            return;
+        }
+        if (password.isEmpty()) {
+            view.showEmptyPasswordError();
+            return;
+        }
+        if (confirmPassword.isEmpty() ){
+            view.showEmptyConfirmPasswordError();
+            return;
+        }
+        if (!password.equals(confirmPassword)){
+            view.showPasswordMismatchError();
+            return;
+        }
 
+        view.showLoadingIndicator();
     }
     @Override
     public void onDestroy(){
