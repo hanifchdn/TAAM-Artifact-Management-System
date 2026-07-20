01fc778f77e45e7e186da0e1d2fcc789755eef12
LackingCuriosity <123216154+LackingCuriosity@users.noreply.github.com>
2026-07-20 12:56:44 -0400

    B07-123 & B07-165 added supabase image uploader that gives the URL into a callback function

--- app/src/main/java/com/example/b07demosummer2024/AddItemFragment.java
+++ app/src/main/java/com/example/b07demosummer2024/AddItemFragment.java
@@ -1,7 +1,9 @@
 package com.example.b07demosummer2024;
 
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,14 +26,28 @@ public class AddItemFragment extends Fragment {
 
     private Uri imageUri;
     private ImageView selectedImagePreview;
+    private SupabaseImageUploader imageUploader;
     private ActivityResultLauncher<String> imageSelectionLauncher =
             registerForActivityResult(new ActivityResultContracts.GetContent(), uri -> {
                         if (uri != null) {
                             imageUri = uri;
                             selectedImagePreview.setImageURI(uri);
+                            imageUploader = new SupabaseImageUploader(getContext());
+
+                            //TODO: Move this example call to where the add button call will be at.
+                            ImageDatabaseWriter imageDatabaseWriter = new ImageDatabaseWriter();
+                            imageDatabaseWriter.addToDatabase(imageUploader, uri, "test2", (url) -> {
+                                if (url == null) {
+                                    Log.d("tst", "oop");
+                                }
+                                else {
+                                    Log.d("tst", url);
+                                }
+                            });
                         }
                     }
             );
+
 
     @Nullable
     @Override
@@ -45,10 +61,13 @@ public class AddItemFragment extends Fragment {
         Button buttonSelectImage = view.findViewById(R.id.buttonSelectImage);
         buttonSelectImage.setOnClickListener(v -> imageSelectionLauncher.launch("image/*"));
         selectedImagePreview = view.findViewById(R.id.selectedImagePreview);
+        Button buttonAddArtifact = view.findViewById(R.id.buttonAddItem);
     }
 
     public Uri getSelectedImageUri() {
         return imageUri;
     }
+    public SupabaseImageUploader getSupabaseImageUploader() {return imageUploader;}
+
 }
 
