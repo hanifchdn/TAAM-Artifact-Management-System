dd3e5bcb3765844ce7a73c8e7d299aa0a4fc0a53
hanifchdn <hanifpietoyo@gmail.com>
2026-07-19 21:51:22 -0400

    feat: add image selection widget

--- app/src/main/java/com/example/b07demosummer2024/AddItemFragment.java
+++ app/src/main/java/com/example/b07demosummer2024/AddItemFragment.java
@@ -1,5 +1,6 @@
 package com.example.b07demosummer2024;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,8 +8,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -17,65 +21,34 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class AddItemFragment extends Fragment {
-    private EditText editTextTitle, editTextAuthor, editTextGenre, editTextDescription;
-    private Spinner spinnerCategory;
-    private Button buttonAdd;
 
-    private FirebaseDatabase db;
-    private DatabaseReference itemsRef;
+    private Uri imageUri;
+    private ImageView selectedImagePreview;
+    private ActivityResultLauncher<String> imageSelectionLauncher =
+            registerForActivityResult(new ActivityResultContracts.GetContent(), uri -> {
+                        if (uri != null) {
+                            imageUri = uri;
+                            selectedImagePreview.setImageURI(uri);
+                        }
+                    }
+            );
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_add_item, container, false);
-
-        editTextTitle = view.findViewById(R.id.editTextTitle);
-        editTextAuthor = view.findViewById(R.id.editTextAuthor);
-        editTextGenre = view.findViewById(R.id.editTextGenre);
-        editTextDescription = view.findViewById(R.id.editTextDescription);
-        spinnerCategory = view.findViewById(R.id.spinnerCategory);
-        buttonAdd = view.findViewById(R.id.buttonAdd);
-
-        db = FirebaseDatabase.getInstance("https://b07-demo-summer-2024-default-rtdb.firebaseio.com/");
-
-        // Set up the spinner with categories
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
-                R.array.categories_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerCategory.setAdapter(adapter);
-
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addItem();
-            }
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_add_item, container, false);
     }
 
-    private void addItem() {
-        String title = editTextTitle.getText().toString().trim();
-        String author = editTextAuthor.getText().toString().trim();
-        String genre = editTextGenre.getText().toString().trim();
-        String description = editTextDescription.getText().toString().trim();
-        String category = spinnerCategory.getSelectedItem().toString().toLowerCase();
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button buttonSelectImage = view.findViewById(R.id.buttonSelectImage);
+        buttonSelectImage.setOnClickListener(v -> imageSelectionLauncher.launch("image/*"));
+        selectedImagePreview = view.findViewById(R.id.selectedImagePreview);
+    }
 
-        if (title.isEmpty() || author.isEmpty() || genre.isEmpty() || description.isEmpty()) {
-            Toast.makeText(getContext(), "Please fill out all fields", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        itemsRef = db.getReference("categories/" + category);
-        String id = itemsRef.push().getKey();
-        Item item = new Item(id, title, author, genre, description);
-
-        itemsRef.child(id).setValue(item).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                Toast.makeText(getContext(), "Item added", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(getContext(), "Failed to add item", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public Uri getSelectedImageUri() {
+        return imageUri;
     }
 }
+
