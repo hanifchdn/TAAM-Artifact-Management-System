602a4a539194ba69e3d77ed253467d183c32347b
Andrew <andrewmatthian08@gmail.com>
2026-07-21 15:33:59 -0400

    fix: modified LoginPresenter to support Model

--- app/src/main/java/com/example/b07demosummer2024/LoginPresenter.java
+++ app/src/main/java/com/example/b07demosummer2024/LoginPresenter.java
@@ -3,12 +3,45 @@ package com.example.b07demosummer2024;
 public class LoginPresenter implements LoginContract.Presenter {
 
     private LoginContract.View view;
-    public LoginPresenter(LoginContract.View view){
+    private LoginContract.Model model;
+    public LoginPresenter(LoginContract.View view, LoginContract.Model model){
         this.view = view;
+        this.model = model;
+    }
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    private boolean isValidEmail(String email){
+        return email.matches(EMAIL_REGEX);
     }
     @Override
-    public void login(String email, String password){
+    public void login(String email, String password) {
+        if (email.isEmpty()) {
+            view.showEmptyEmailError();
+            return;
+        }
 
+        if (password.isEmpty()) {
+            view.showEmptyPasswordError();
+            return;
+        }
+
+        if (!isValidEmail(email)) {
+            view.showInvalidEmailFormatError();
+            return;
+        }
+
+        model.login(email, password, new LoginContract.Model.AuthCallback() {
+            @Override
+            public void onSuccess(String uid) {
+                if (view == null) return;      // Activity may be gone
+                // navigate View to success/home page - to be implemented
+            }
+            @Override
+            public void onFailure(String message) {
+                if (view == null) return;
+                // naviagte View to error - to be implemented
+            }
+        });
     }
     @Override
     public void onDestroy(){
