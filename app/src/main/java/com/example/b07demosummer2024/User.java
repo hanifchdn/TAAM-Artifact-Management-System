0ae59787503fff76360e678ecd74151faeb02f03
Andrew <andrewmatthian08@gmail.com>
2026-07-21 22:36:49 -0400

    fix: removed username field

--- app/src/main/java/com/example/b07demosummer2024/User.java
+++ app/src/main/java/com/example/b07demosummer2024/User.java
@@ -1,7 +1,7 @@
 package com.example.b07demosummer2024;
 
 public class User {
-    private String username;
+
     private String email;
     private String uid;
     private boolean isAdmin;
@@ -11,16 +11,12 @@ public class User {
     }
 
     public User(String username, String email, String uid) {
-        this.username = username;
         this.email = email;
         this.uid = uid;
         this.isAdmin = false;
     }
     public String getUid(){
         return uid;
-    }
-    public String getUsername() {
-        return username;
     }
     public String getEmail() {
         return email;
