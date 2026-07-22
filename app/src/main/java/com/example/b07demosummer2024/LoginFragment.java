db4ecac2fca9b892b72d9e23b3422a7ed9f2b89a
hanifchdn <hanifpietoyo@gmail.com>
2026-07-21 23:45:19 -0400

    fix: moved login implementation to its own fragment

---
+++ app/src/main/java/com/example/b07demosummer2024/LoginFragment.java
@@ -0,0 +1,72 @@
+package com.example.b07demosummer2024;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import android.widget.EditText;
+
+
+public class LoginFragment extends Fragment {
+    private Button loginButton;
+    private boolean isPasswordVisible = false;
+    private EditText usernameInput;
+    private EditText passwordInput;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        usernameInput = view.findViewById(R.id.username_input);
+        passwordInput = view.findViewById(R.id.password_input);
+        loginButton = view.findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(v -> {
+            loadFragment(new HomeFragment());
+        });
+
+        passwordInput.setOnTouchListener((v, event) -> {
+            final int DRAWABLE_END = 2;
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                if (event.getRawX() >= (passwordInput.getRight()
+                        - passwordInput.getCompoundDrawables()[DRAWABLE_END].getBounds().width()
+                        - passwordInput.getPaddingEnd())) {
+                    togglePasswordVisibility();
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+
+    private void togglePasswordVisibility() {
+        isPasswordVisible = !isPasswordVisible;
+        int cursorPosition = passwordInput.getSelectionEnd();
+        if (isPasswordVisible) {
+            passwordInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+        } else {
+            passwordInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        }
+        passwordInput.setSelection(cursorPosition);
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
--- app/src/main/java/com/example/b07demosummer2024/MainActivity.java
+++ app/src/main/java/com/example/b07demosummer2024/MainActivity.java
@@ -10,12 +10,9 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.widget.EditText;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
-import android.text.InputType;
-import android.view.MotionEvent;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -23,11 +20,6 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase db;
-
-    private Button loginButton;
-    private boolean isPasswordVisible = false;
-    private EditText usernameInput;
-    private EditText passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,39 +32,9 @@ public class MainActivity extends AppCompatActivity {
 //        myRef.setValue("B07 Demo!");
         myRef.child("movies").setValue("B07 Demo!");
 
-
-        usernameInput = findViewById(R.id.username_input);
-        passwordInput = findViewById(R.id.password_input);
-        loginButton = findViewById(R.id.loginButton);
-
-        loginButton.setOnClickListener(v -> {
-            loadFragment(new HomeFragment());
-        });
-
-        passwordInput.setOnTouchListener((v, event) -> {
-            final int DRAWABLE_END = 2;
-            if (event.getAction() == MotionEvent.ACTION_UP) {
-                if (event.getRawX() >= (passwordInput.getRight()
-                        - passwordInput.getCompoundDrawables()[DRAWABLE_END].getBounds().width()
-                        - passwordInput.getPaddingEnd())) {
-                    togglePasswordVisibility();
-                    return true;
-                }
-            }
-            return false;
-        });
-
-    }
-
-    private void togglePasswordVisibility() {
-        isPasswordVisible = !isPasswordVisible;
-        int cursorPosition = passwordInput.getSelectionEnd();
-        if (isPasswordVisible) {
-            passwordInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-        } else {
-            passwordInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        if (savedInstanceState == null) {
+            loadFragment(new LoginFragment());
         }
-        passwordInput.setSelection(cursorPosition);
     }
 
     private void loadFragment(Fragment fragment) {
