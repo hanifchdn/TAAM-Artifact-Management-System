526da6d37c036eff21b498bcc3a5a5f42c84ddcc
hanifchdn <hanifpietoyo@gmail.com>
2026-07-28 23:05:48 -0400

    bugfix: fix wrong file to be committed B07-31

--- app/src/main/java/com/example/b07demosummer2024/HomeFragment.java
+++ app/src/main/java/com/example/b07demosummer2024/HomeFragment.java
@@ -3,9 +3,13 @@ package com.example.b07demosummer2024;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,6 +18,9 @@ import androidx.fragment.app.FragmentTransaction;
 import java.util.concurrent.CompletableFuture;
 
 public class HomeFragment extends Fragment {
+    private ImageButton searchButton;
+    private EditText searchInput;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -51,6 +58,30 @@ public class HomeFragment extends Fragment {
 //        });
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        /**
+         * Binds variables from xml layout to java
+         */
+        searchButton = view.findViewById(R.id.searchButton);
+        searchInput = view.findViewById(R.id.searchInput);
+
+        /**
+         * Displays searchInput when searchButton is clicked
+         */
+        searchButton.setOnClickListener(v -> {
+            if (searchInput.getVisibility() == View.GONE) {
+                searchInput.setVisibility(View.VISIBLE);
+                searchInput.requestFocus();
+            }
+            else {
+                searchInput.setVisibility(View.GONE);
+            }
+        });
     }
 
     private void loadFragment(Fragment fragment) {
