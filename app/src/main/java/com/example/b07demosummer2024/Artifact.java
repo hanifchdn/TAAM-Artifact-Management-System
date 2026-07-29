a874fcf9132ad52f04075197278875e299feff7e
LackingCuriosity <123216154+LackingCuriosity@users.noreply.github.com>
2026-07-28 23:11:49 -0400

    feat:B07-198/200 updated Artifact class to store liked user and associated methods

--- app/src/main/java/com/example/b07demosummer2024/Artifact.java
+++ app/src/main/java/com/example/b07demosummer2024/Artifact.java
@@ -1,5 +1,9 @@
 package com.example.b07demosummer2024;
 
+import com.google.firebase.database.Exclude;
+
+import java.util.ArrayList;
+
 /**
  * Artifact class that defines the Artifact structure
  *
@@ -26,6 +30,7 @@ public class Artifact implements DatabaseItem{
     private String accessionNumber;
     private String notes;
     private String imageUrl;
+    private ArrayList<String> likedUsers;
 
     public Artifact() {}
     public Artifact(String LOT, String name, String description, String category, String material, String dynasty) {
@@ -35,7 +40,57 @@ public class Artifact implements DatabaseItem{
         this.category = category;
         this.material = material;
         this.dynasty = dynasty;
+        this.likedUsers = new ArrayList<String>();
 
+    }
+
+    /**
+     * Returns if a user has already liked
+     * @param UID of user to check if they have liked or not
+     * @return true/false boolean if they liked or not
+     */
+    @Exclude
+    public boolean isUserLiked(String UID) {
+        return likedUsers.contains(UID);
+    }
+
+    /**
+     * Removes a UID to the list of users who has liked this artifact.
+     * If the UID is NOT already present, returns -1. Else, remove and return -1
+     * Note this will NOT update the db
+     * @param UID of the user to remove
+     * @return -1 if user is NOT already in the list, else on success returns 0
+     */
+    public int removeLikedUser(String UID) {
+        if (likedUsers.contains(UID)) {
+            return -1;
+        }
+        likedUsers.add(UID);
+        return 0;
+    }
+
+    /**
+     * Adds a UID to the list of users who has liked this artifact.
+     * If the UID is already present, then it will not add it, and returns -1.
+     * Note this will NOT update the db
+     * @param UID of the user to add
+     * @return -1 if user is already in the list, else on success returns 0
+     */
+    public int addLikedUser(String UID) {
+        if (likedUsers.contains(UID)) {
+            return -1;
+        }
+        likedUsers.add(UID);
+        return 0;
+    }
+
+    /**
+     * Returns the total amount of likes on this artifact
+     * @return number of likes on this artifact
+     */
+    @Exclude
+    public int getTotalLikes() {
+        return likedUsers.size();
     }
     public String getLOT() {
         return LOT;
@@ -167,6 +222,14 @@ public class Artifact implements DatabaseItem{
 
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
+    }
+
+    public ArrayList<String> getLikedUsers() {
+        return likedUsers;
+    }
+
+    public void setLikedUsers(ArrayList<String> likedUsers) {
+        this.likedUsers = likedUsers;
     }
 
 }
