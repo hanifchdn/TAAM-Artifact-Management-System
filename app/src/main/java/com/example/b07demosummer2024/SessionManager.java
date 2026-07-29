242ac219b506a499d7a06681f9e6664f644990ac
Andrew <andrewmatthian08@gmail.com>
2026-07-29 17:51:20 -0400

    feat: implemented SessionManager

--- app/src/main/java/com/example/b07demosummer2024/SessionManager.java
+++ app/src/main/java/com/example/b07demosummer2024/SessionManager.java
@@ -1,4 +1,25 @@
 package com.example.b07demosummer2024;
 
 public class SessionManager {
+    private static SessionManager instance;
+    private User currentUser;
+    private SessionManager() {
+
+    }
+    public static synchronized SessionManager getInstance() {
+        if (instance == null) instance = new SessionManager();
+        return instance;
+    }
+    public void setCurrentUser(User u){
+        currentUser = u;
+    }
+    public User getCurrentUser(){
+        return currentUser;
+    }
+    public boolean isAdmin(){
+        return currentUser != null && currentUser.isAdmin();
+    }
+    public void clear(){
+        currentUser = null;
+    }
 }
