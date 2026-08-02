8334b163fb83efa808ea7a199f569838515f2b15
LackingCuriosity <123216154+LackingCuriosity@users.noreply.github.com>
2026-08-02 16:13:36 -0400

    feat: B07-262/263 updated user model to contain saved artifact lots, and added userdatabase connector

--- app/src/main/java/com/example/b07demosummer2024/User.java
+++ app/src/main/java/com/example/b07demosummer2024/User.java
@@ -1,11 +1,15 @@
 package com.example.b07demosummer2024;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
     private String username;
     private String email;
     private String uid;
     private boolean isAdmin;
+    private List<String> savedArtifactList;
 
     public User(){
 
@@ -16,12 +20,52 @@ public class User {
         this.email = email;
         this.uid = uid;
         this.isAdmin = isAdmin;
+        this.savedArtifactList = new ArrayList<>();
     }
 
+    /**
+     * Deletes an artifact lot to the saved artifact list
+     * If the artifact lot is already in there, nothing is removed, no error occurs
+     * @param lot of artifact to remove
+     */
+    public void removeSavedArtifact(String lot) {
+        savedArtifactList.remove(lot);
+    }
+
+    /**
+     * Adds an artifact lot to the saved artifact list
+     * If artifact lot is already in there, a duplicate will not be added
+     * @param lot of artifact to add
+     */
+    public void addSavedArtifact(String lot) {
+        if (containsSavedArtifact(lot)) {
+            return;
+        }
+        if (savedArtifactList == null) {
+            savedArtifactList = new ArrayList<String>();
+        }
+        savedArtifactList.add(lot);
+    }
+
+    /**
+     * Returns if an artifact is in the saved artifact list
+     * @param lot of artifact to check
+     * @return true iff artifactList contains the lot
+     */
+    public boolean containsSavedArtifact(String lot) {
+        if (savedArtifactList == null) {
+            return false;
+        }
+        return savedArtifactList.contains(lot);
+    }
+
+    public void setSavedArtifactList(List<String> savedArtifactList) {
+        this.savedArtifactList = savedArtifactList;
+    }
+    public List<String> getSavedArtifactList() {return savedArtifactList; }
     public String getUsername() {
         return username;
     }
-
     public String getUid(){
         return uid;
     }
@@ -31,4 +75,5 @@ public class User {
     public boolean isAdmin() {
         return isAdmin;
     }
+
 }
---
+++ app/src/main/java/com/example/b07demosummer2024/UserDatabaseWriter.java
@@ -0,0 +1,55 @@
+package com.example.b07demosummer2024;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.core.WriteTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Takes in a User class and will ONLY update user preference/collections/likes etc
+ */
+public class UserDatabaseWriter {
+    private FirebaseDatabase db;
+    private DatabaseReference dbReference;
+    public UserDatabaseWriter() {
+        db = FirebaseDatabase.getInstance("https://taam-artifact-storage-system-default-rtdb.firebaseio.com/");
+        dbReference = db.getReference();
+    }
+
+    /**
+     * Updates the savedArtifacts of a given useruid
+     * @param userUid of the user to update savedArtifactList
+     * @param savedArtifactList of the user to update
+     * @param callback on success/failure of database query
+     */
+    public void updateSavedArtifacts(String userUid, List<String> savedArtifactList, WriteCallback callback) {
+        dbReference.child(userUid).child("savedArtifacts").setValue(savedArtifactList).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                callback.onSuccess();
+                return;
+            }
+            else {
+                if (task.getException() == null) {
+                    callback.onFailure("An unknown error occured");
+                    return;
+                }
+                callback.onFailure(task.getException().toString());
+                return;
+            }
+        });
+    }
+
+
+    /**
+     * Updates the savedArtifacts of a given useruid
+     * @param user object on which to update savedArtifacts in database
+     * @param callback on success/failure of database query
+     */
+    public void updateSavedArtifacts(User user, WriteCallback callback) {
+        updateSavedArtifacts(user.getUid(), user.getSavedArtifactList(), callback);
+    }
+
+
+}
