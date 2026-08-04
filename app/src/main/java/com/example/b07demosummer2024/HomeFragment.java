93578d4b4a42e578c3d31d995d9fcfbfd7766c53
Andrew <andrewmatthian08@gmail.com>
2026-08-03 20:36:53 -0400

    feat: hide add artifact for non-admin

--- app/src/main/java/com/example/b07demosummer2024/HomeFragment.java
+++ app/src/main/java/com/example/b07demosummer2024/HomeFragment.java
@@ -169,6 +169,11 @@ public class HomeFragment extends Fragment {
         ImageButton addArtifactButton =
                 view.findViewById(R.id.addArtifactButton);
 
+
+        addArtifactButton.setVisibility(
+                isAdmin ? View.VISIBLE : View.GONE
+        );
+
         addArtifactButton.setOnClickListener(
                 v -> loadFragment(new AddItemFragment())
         );
